abb2facbda144dea56e84bdd810d4e355c43588a
PaulineRollier <pauline.rollier@alten.com>
2023-06-12 12:09:03 +0200

    Ajout workflow pytest

--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ setup(
     name="pkg_test_mathematique_testpi_2",
     version=mathematique.__version__,
     packages=["mathematique"],
-    description="demonstration de creation d'un package Python",
-    long_description=open(os.path.join(os.path.dirname(__file__), "README.txt")).read(),
     install_requires=["pytest", "sphinx", "sphinx_rtd_theme", "ghp-import"],
 )
