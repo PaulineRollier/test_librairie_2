cd1b7d41239fb7e4f4e33f8a500982e84b97b9c0
PaulineRollier <pauline.rollier@alten.com>
2023-07-13 15:05:50 +0200

    Readme rst

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 import mathematique
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+# try:
+#     import pypandoc
+#     long_description = pypandoc.convert_file('README.md', 'rst')
+# except(IOError, ImportError):
+    # long_description = open('README.md').read()
         
 setup(
     name="pkg_test_mathematique_testpi_2",
     version=mathematique.__version__,
     packages=["mathematique"],
     description="A User Parameter-free Bayesian Framework for Uplift Modeling",
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     long_description_content_type='text/markdown',
     install_requires=["pytest", "sphinx", "sphinx_rtd_theme", "ghp-import"],
 )
