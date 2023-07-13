1941c1e64c083431d645bf059cac4e8872a886c8
PaulineRollier <pauline.rollier@alten.com>
2023-07-13 16:41:13 +0200

    Changement setup

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup
 import mathematique
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
-        
 setup(
     name="pkg_test_mathematique_testpi_2",
     version=mathematique.__version__,
     packages=["mathematique"],
     description="A User Parameter-free Bayesian Framework for Uplift Modeling",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     install_requires=["pytest", "sphinx", "sphinx_rtd_theme", "ghp-import"],
 )
